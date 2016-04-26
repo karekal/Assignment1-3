da6d77a0b8981fbfa88be914c62f0a5524f9fcd0
srima karekal <srima589.karekal@gmail.com>
2016-04-26 20:45:24 +0530

    Assignment1-3

---
+++ src/Assignment1_3.java
@@ -0,0 +1,11 @@
+
+public class Assignment1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int x= 4.5;
+	     float y='a';
+	     System.out.println(y);
+	}
+
+}
